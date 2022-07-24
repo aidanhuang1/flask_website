c16bde9fc9cf502e5cf50f7eef9cd13cf63445d3
Aidan Huang <aidanhuangsiqi@gmail.com>
2022-07-23 20:35:50 -0400

    commit

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-	return ("")
+def home():
+    return render_template('index.html')
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        print(user)
+        return redirect(url_for('user', usr=user))
+    else:
+        return render_template('login.html')
+
+@app.route('/<usr>')
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
