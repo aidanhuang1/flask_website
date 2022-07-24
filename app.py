ea12ce682568d5a8ba76566b2141caace92b4071
Aidan Huang <aidanhuangsiqi@gmail.com>
2022-07-24 09:35:31 -0400

    python

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = 'secretkey'
+app.permanet_session_lifetime = timedelta(minutes=5)
 
 @app.route('/')
 def home():
@@ -9,15 +12,29 @@ def home():
 @app.route("/login", methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
+        session.permanent = True
         user = request.form['nm']
-        print(user)
-        return redirect(url_for('user', usr=user))
+        # print(user)
+        session['user'] = user 
+        return redirect(url_for('user'))
     else:
+        if 'user' in session:
+            return redirect(url_for('user'))
         return render_template('login.html')
 
-@app.route('/<usr>')
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route('/user')
+def user():
+    if 'user' in session:
+        user = session['user']
+        return f"<h1>{user}</h1>"
+    else: #if the user is not in the session, has to login again, etc...
+        return redirect(url_for('login'))
+    
+@app.route('/logout')
+def logout():
+    flash('you have been logged out', 'info')
+    session.pop('user', None) #remove user session
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
