f14d116f0462c46b3ed4ceb6b19411ed3695ec0d
Aidan Huang <aidanhuangsiqi@gmail.com>
2022-07-25 17:07:18 -0400

    firebase

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+from second import second
+import pyrebase
 
 app = Flask(__name__)
-app.secret_key = 'secretkey'
+
+config = {
+    'apiKey': "AIzaSyCghngurNTrJ4jlLcxe5cvLFLr7frvN6V8",
+    'authDomain': "authentication-flask-website.firebaseapp.com",
+    'projectId': "authentication-flask-website",
+    'storageBucket': "authentication-flask-website.appspot.com",
+    'messagingSenderId': "671826927467",
+    'appId': "1:671826927467:web:24ce92e9ddd9d0377b362e",
+    'measurementId': "G-T48NEMQW4M",
+    'databaseURL': ''
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+app.register_blueprint(second, url_prefix="")
+app.secret_key = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
@@ -31,29 +49,37 @@ def home():
 def view():
     return render_template('view.html', values=users.query.all())
 
-@app.route("/login", methods=['POST', 'GET'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if 'user' in session:
+        flash("Already logged in")
+        return redirect(url_for('user'))
     if request.method == 'POST':
         session.permanent = True
-        user = request.form['nm']
-        session['user'] = user 
+        # user = request.form['nm']
+        # session['user'] = user 
         
-        found_user = users.query.filter_by(name=user).first()
-        if found_user: #user is found in database
-            session['email'] = found_user.email
-        else: #if user doesn't exist in database
-            usr = users(user, '')
-            db.session.add(usr)
-            db.session.commit()
-        
-        flash('login successful!')
-        return redirect(url_for('user'))
-    else:
-        if 'user' in session:
-            flash("Already logged in")
+        # found_user = users.query.filter_by(name=user).first()
+        # if found_user: 
+        #     #user is found in database
+        #     session['email'] = found_user.email
+        # else: 
+        #     #if user doesn't exist in database
+        #     usr = users(user, '')
+        #     db.session.add(usr)
+        #     db.session.commit()
+        email = request.form.get('email')
+        password = request.form.get('password') 
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            session['user'] = email
+            print(user)
+            
+            flash('login successful!')
             return redirect(url_for('user'))
-        return render_template('login.html')
-
+        except:
+            flash('failed to login')        
+    
 @app.route('/user', methods=['POST', 'GET'])
 def user():
     email = None
